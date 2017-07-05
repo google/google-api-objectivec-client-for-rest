89ed21effe1c997bb2202ae43e4ea9586d38a292
Thomas Van Lenten <thomasvl@google.com>
2017-07-05 16:24:00 -0400

    Generate current sources to pick up updated "People" service.

--- Source/GeneratedServices/PeopleService/GTLRPeopleServiceQuery.m
+++ Source/GeneratedServices/PeopleService/GTLRPeopleServiceQuery.m
@@ -30,6 +30,155 @@ NSString * const kGTLRPeopleServiceSortOrderLastNameAscending  = @"LAST_NAME_ASC
 
 @end
 
+@implementation GTLRPeopleServiceQuery_ContactGroupsBatchGet
+
+@dynamic maxMembers, resourceNames;
+
++ (NSDictionary<NSString *, Class> *)arrayPropertyToClassMap {
+  NSDictionary<NSString *, Class> *map = @{
+    @"resourceNames" : [NSString class]
+  };
+  return map;
+}
+
++ (instancetype)query {
+  NSString *pathURITemplate = @"v1/contactGroups:batchGet";
+  GTLRPeopleServiceQuery_ContactGroupsBatchGet *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:nil
+                       pathParameterNames:nil];
+  query.expectedObjectClass = [GTLRPeopleService_BatchGetContactGroupsResponse class];
+  query.loggingName = @"people.contactGroups.batchGet";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_ContactGroupsCreate
+
++ (instancetype)queryWithObject:(GTLRPeopleService_CreateContactGroupRequest *)object {
+  if (object == nil) {
+    GTLR_DEBUG_ASSERT(object != nil, @"Got a nil object");
+    return nil;
+  }
+  NSString *pathURITemplate = @"v1/contactGroups";
+  GTLRPeopleServiceQuery_ContactGroupsCreate *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:@"POST"
+                       pathParameterNames:nil];
+  query.bodyObject = object;
+  query.expectedObjectClass = [GTLRPeopleService_ContactGroup class];
+  query.loggingName = @"people.contactGroups.create";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_ContactGroupsDelete
+
+@dynamic deleteContacts, resourceName;
+
++ (instancetype)queryWithResourceName:(NSString *)resourceName {
+  NSArray *pathParams = @[ @"resourceName" ];
+  NSString *pathURITemplate = @"v1/{+resourceName}";
+  GTLRPeopleServiceQuery_ContactGroupsDelete *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:@"DELETE"
+                       pathParameterNames:pathParams];
+  query.resourceName = resourceName;
+  query.expectedObjectClass = [GTLRPeopleService_Empty class];
+  query.loggingName = @"people.contactGroups.delete";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_ContactGroupsGet
+
+@dynamic maxMembers, resourceName;
+
++ (instancetype)queryWithResourceName:(NSString *)resourceName {
+  NSArray *pathParams = @[ @"resourceName" ];
+  NSString *pathURITemplate = @"v1/{+resourceName}";
+  GTLRPeopleServiceQuery_ContactGroupsGet *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:nil
+                       pathParameterNames:pathParams];
+  query.resourceName = resourceName;
+  query.expectedObjectClass = [GTLRPeopleService_ContactGroup class];
+  query.loggingName = @"people.contactGroups.get";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_ContactGroupsList
+
+@dynamic pageSize, pageToken, syncToken;
+
++ (instancetype)query {
+  NSString *pathURITemplate = @"v1/contactGroups";
+  GTLRPeopleServiceQuery_ContactGroupsList *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:nil
+                       pathParameterNames:nil];
+  query.expectedObjectClass = [GTLRPeopleService_ListContactGroupsResponse class];
+  query.loggingName = @"people.contactGroups.list";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_ContactGroupsMembersModify
+
+@dynamic resourceName;
+
++ (instancetype)queryWithObject:(GTLRPeopleService_ModifyContactGroupMembersRequest *)object
+                   resourceName:(NSString *)resourceName {
+  if (object == nil) {
+    GTLR_DEBUG_ASSERT(object != nil, @"Got a nil object");
+    return nil;
+  }
+  NSArray *pathParams = @[ @"resourceName" ];
+  NSString *pathURITemplate = @"v1/{+resourceName}/members:modify";
+  GTLRPeopleServiceQuery_ContactGroupsMembersModify *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:@"POST"
+                       pathParameterNames:pathParams];
+  query.bodyObject = object;
+  query.resourceName = resourceName;
+  query.expectedObjectClass = [GTLRPeopleService_ModifyContactGroupMembersResponse class];
+  query.loggingName = @"people.contactGroups.members.modify";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_ContactGroupsUpdate
+
+@dynamic resourceName;
+
++ (instancetype)queryWithObject:(GTLRPeopleService_UpdateContactGroupRequest *)object
+                   resourceName:(NSString *)resourceName {
+  if (object == nil) {
+    GTLR_DEBUG_ASSERT(object != nil, @"Got a nil object");
+    return nil;
+  }
+  NSArray *pathParams = @[ @"resourceName" ];
+  NSString *pathURITemplate = @"v1/{+resourceName}";
+  GTLRPeopleServiceQuery_ContactGroupsUpdate *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:@"PUT"
+                       pathParameterNames:pathParams];
+  query.bodyObject = object;
+  query.resourceName = resourceName;
+  query.expectedObjectClass = [GTLRPeopleService_ContactGroup class];
+  query.loggingName = @"people.contactGroups.update";
+  return query;
+}
+
+@end
+
 @implementation GTLRPeopleServiceQuery_PeopleConnectionsList
 
 @dynamic pageSize, pageToken, personFields, requestMaskIncludeField,
@@ -49,6 +198,47 @@ NSString * const kGTLRPeopleServiceSortOrderLastNameAscending  = @"LAST_NAME_ASC
   query.resourceName = resourceName;
   query.expectedObjectClass = [GTLRPeopleService_ListConnectionsResponse class];
   query.loggingName = @"people.people.connections.list";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_PeopleCreateContact
+
+@dynamic parent;
+
++ (instancetype)queryWithObject:(GTLRPeopleService_Person *)object {
+  if (object == nil) {
+    GTLR_DEBUG_ASSERT(object != nil, @"Got a nil object");
+    return nil;
+  }
+  NSString *pathURITemplate = @"v1/people:createContact";
+  GTLRPeopleServiceQuery_PeopleCreateContact *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:@"POST"
+                       pathParameterNames:nil];
+  query.bodyObject = object;
+  query.expectedObjectClass = [GTLRPeopleService_Person class];
+  query.loggingName = @"people.people.createContact";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_PeopleDeleteContact
+
+@dynamic resourceName;
+
++ (instancetype)queryWithResourceName:(NSString *)resourceName {
+  NSArray *pathParams = @[ @"resourceName" ];
+  NSString *pathURITemplate = @"v1/{+resourceName}:deleteContact";
+  GTLRPeopleServiceQuery_PeopleDeleteContact *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:@"DELETE"
+                       pathParameterNames:pathParams];
+  query.resourceName = resourceName;
+  query.expectedObjectClass = [GTLRPeopleService_Empty class];
+  query.loggingName = @"people.people.deleteContact";
   return query;
 }
 
@@ -100,6 +290,31 @@ NSString * const kGTLRPeopleServiceSortOrderLastNameAscending  = @"LAST_NAME_ASC
                        pathParameterNames:nil];
   query.expectedObjectClass = [GTLRPeopleService_GetPeopleResponse class];
   query.loggingName = @"people.people.getBatchGet";
+  return query;
+}
+
+@end
+
+@implementation GTLRPeopleServiceQuery_PeopleUpdateContact
+
+@dynamic resourceName, updatePersonFields;
+
++ (instancetype)queryWithObject:(GTLRPeopleService_Person *)object
+                   resourceName:(NSString *)resourceName {
+  if (object == nil) {
+    GTLR_DEBUG_ASSERT(object != nil, @"Got a nil object");
+    return nil;
+  }
+  NSArray *pathParams = @[ @"resourceName" ];
+  NSString *pathURITemplate = @"v1/{+resourceName}:updateContact";
+  GTLRPeopleServiceQuery_PeopleUpdateContact *query =
+    [[self alloc] initWithPathURITemplate:pathURITemplate
+                               HTTPMethod:@"PATCH"
+                       pathParameterNames:pathParams];
+  query.bodyObject = object;
+  query.resourceName = resourceName;
+  query.expectedObjectClass = [GTLRPeopleService_Person class];
+  query.loggingName = @"people.people.updateContact";
   return query;
 }
 
